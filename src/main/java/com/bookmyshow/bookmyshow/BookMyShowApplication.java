2a69e09a62b8cca0096253a4c2cf8f672347b274
Swatantra Prakash <swatantra.prakash@karexpert.com>
2024-01-04 07:46:30 +0530

    Book My Show Code Added

--- src/main/java/com/bookmyshow/bookmyshow/BookMyShowApplication.java
+++ src/main/java/com/bookmyshow/bookmyshow/BookMyShowApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookMyShowApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookMyShowApplication.class, args);
 	}
