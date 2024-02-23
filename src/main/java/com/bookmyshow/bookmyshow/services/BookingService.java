dc5d78280a40e0d6df66a74636fa44c3f52eebc3
Swatantra Prakash <swatantra.prakash@karexpert.com>
2024-02-23 14:55:07 +0530

    Book My Show Code Added

--- src/main/java/com/bookmyshow/bookmyshow/services/BookingService.java
+++ src/main/java/com/bookmyshow/bookmyshow/services/BookingService.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class BookingService {
     private UserRepository userRepository;
@@ -23,6 +24,7 @@ public class BookingService {
     private ShowSeatRepository showSeatRepository;
     private PriceCalculatorService priceCalculatorService;
     private BookingRepository bookingRepository;
+
     @Autowired
     public BookingService(UserRepository userRepository, ShowRepository showRepository, ShowSeatRepository showSeatRepository, PriceCalculatorService priceCalculatorService, BookingRepository bookingRepository) {
         this.userRepository = userRepository;
@@ -33,42 +35,42 @@ public class BookingService {
     }
 
     @Transactional
-    public Booking bookMovie (Long userId, List<Long> seatIds,Long showId){
+    public Booking bookMovie(Long userId, List<Long> seatIds, Long showId) {
         //1.Get the User with userId
         Optional<User> userOptional = userRepository.findById(userId);
-        if(userOptional.isEmpty()){
+        if (userOptional.isEmpty()) {
             throw new UserNotFoundException();
         }
         User user = userOptional.get();
         //2.Get the show with ShowId
         Optional<Show> showOptional = showRepository.findById(showId);
-        if(showOptional.isEmpty()){
+        if (showOptional.isEmpty()) {
             throw new RuntimeException();
         }
         Show bookedShow = showOptional.get();
         //----------Start Transaction-----------
         //3.Get the seats with seatIds provided
         List<ShowSeat> showSeatList = showSeatRepository.findAllById(seatIds);
-        for(ShowSeat showSeat:showSeatList){
-            if(!showSeat.getShowSeatStatus().equals(ShowSeatStatus.AVAILABLE) ||
+        for (ShowSeat showSeat : showSeatList) {
+            if (!showSeat.getShowSeatStatus().equals(ShowSeatStatus.AVAILABLE) ||
                     (showSeat.getShowSeatStatus().equals(ShowSeatStatus.BLOCKED)) ||
-                            (Duration.between(showSeat.getBlockedAt().toInstant(),new Date().toInstant()).toMinutes()>15
-               )){
+                    (Duration.between(showSeat.getBlockedAt().toInstant(), new Date().toInstant()).toMinutes() > 15
+                    )) {
                 throw new RuntimeException();
             }
         }
         //4.check if the seat are available
         //5.if no, return error
         //6.if yes,mark the status of seat as locked
-        List<ShowSeat> saveShowSeats=new ArrayList<>();
-        for (ShowSeat showSeat:showSeatList){
+        List<ShowSeat> saveShowSeats = new ArrayList<>();
+        for (ShowSeat showSeat : showSeatList) {
             showSeat.setShowSeatStatus(ShowSeatStatus.BLOCKED);
             saveShowSeats.add(showSeatRepository.save(showSeat));
         }
         //7.save the updated seat status in DB
         //----------End Transaction-----------
         //8.create corresponding booking object
-        Booking booking=new Booking();
+        Booking booking = new Booking();
         booking.setBookingStatus(BookingStatus.PENDING);
         booking.setShowSeats(saveShowSeats);
         booking.setUser(user);
